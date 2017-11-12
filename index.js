cae7d1c8c2a82be7c5cf6100629fa7ddca871e67
Osbaldo Martinez Jr <martinez.osbaldo@gmail.com>
2017-11-12 03:28:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function prependDriver(x){
   return newArray;
 }
 function removeLastDriver(){
-    const newArray = drivers.slice(1);
+    const newArray = drivers.slice(3);
     return newArray;
 }
